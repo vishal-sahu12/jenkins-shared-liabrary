0a8b2df90d1367a3498bfd1e1ef329067f75e608
Vishal Sahu <sahuvishal2712da@gmail.com>
2026-03-28 22:27:04 +0530

    added git push

---
+++ vars/code_push.groovy
@@ -0,0 +1,23 @@
+def call(){
+    echo "This is code push file"
+    withCredentials([usernamePassword(
+                    credentialsId:'Github',
+                   gitToolName: 'Default'
+                    )]){
+               sh '''
+                        echo "Checking repository status: "
+                        git status
+                    
+                        echo "Adding changes to git: "
+                        git add .
+                        
+                        echo "Commiting changes: "
+                        git commit -m "Updated environment variables"
+                        
+                        echo "Pushing changes to github: "
+                        git push https://github.com/LondheShubham153/Wanderlust-Mega-Project.git main
+                    '''
+              
+}
+
+}
