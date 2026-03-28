c724202f448d7f2bdeb49a796e693967c40e7361
Vishal Sahu <68978850+vishal-sahu12@users.noreply.github.com>
2026-03-28 19:13:18 +0530

    Fix syntax for credentialsId and usernameVariable

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,8 +1,8 @@
 def call (String ImageName,String Version){
 echo "This is Build File"
 withCredentials([usernamePassword(
-                    credentialsId:"dockerhub",
-                    usernameVariable:"dockerHubUser"
+                    credentialsId:'dockerhub',
+                    usernameVariable:'dockerHubUser'
                     )]){
                 sh "docker build -t ${env.dockerHubUser}/${ImageName}:${Version} ."
               
