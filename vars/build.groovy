b3f3d3987cf41b8932aa5bd980b80e0daef886a2
Vishal Sahu <68978850+vishal-sahu12@users.noreply.github.com>
2026-03-28 19:24:24 +0530

    Enhance Docker build with password variable
    Added password variable for Docker Hub credentials.

--- vars/build.groovy
+++ vars/build.groovy
@@ -2,7 +2,8 @@ def call (String ImageName,String Version){
 echo "This is Build File"
 withCredentials([usernamePassword(
                     credentialsId:'dockerhub',
-                    usernameVariable:'dockerHubUser'
+                    usernameVariable:'dockerHubUser',
+                    passwordVariable: 'dockerHubPass'
                     )]){
                 sh "docker build -t ${env.dockerHubUser}/${ImageName}:${Version} ."
               
