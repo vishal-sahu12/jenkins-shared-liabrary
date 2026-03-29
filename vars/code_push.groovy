1aeb1b01dffe22981314d35d8136f346c257a311
Vishal Sahu <sahuvishal2712da@gmail.com>
2026-03-29 12:36:57 +0530

    updated username

--- vars/code_push.groovy
+++ vars/code_push.groovy
@@ -1,24 +1,22 @@
 def call(){
     echo "This is code push file"
     withCredentials([usernamePassword(
-                    credentialsId:'Github',
-                    usernameVariable:"gitUser", 
-                    passwordVariable:"gitPass"
-                    )]){
-               sh '''
-                        echo "Checking repository status: "
-                        git status
-                    
-                        echo "Adding changes to git: "
-                        git add .
-                        
-                        echo "Commiting changes: "
-                        git commit -m "Updated environment variables"
-                        
-                        echo "Pushing changes to github: "
-                        git push https://github.com/vishal-sahu12/Wanderlust-Mega-Project.git main
-                    '''
-              
-}
+        credentialsId: 'Github',
+        usernameVariable: 'gitUser',
+        passwordVariable: 'gitPass'
+    )]) {
+        sh '''
+            echo "Checking repository status:"
+            git status
 
+            echo "Adding changes:"
+            git add .
+
+            echo "Committing changes:"
+            git commit -m "Updated environment variables"
+
+            echo "Pushing to GitHub:"
+            git push https://${gitUser}:${gitPass}@github.com/vishal-sahu12/Wanderlust-Mega-Project.git main
+        '''
+    }
 }
